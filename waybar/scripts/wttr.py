5d155086cbbb9d9b776a017c033700c31a480c17
Kiize <paolozzobibi@gmail.com>
2025-07-28 11:19:17 +0200

    update

--- waybar/scripts/wttr.py
+++ waybar/scripts/wttr.py
@@ -1,64 +1,64 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import json
 import requests
 from datetime import datetime
 
 WEATHER_CODES = {
-    '113': '☀️',
-    '116': '⛅️',
-    '119': '☁️',
-    '122': '☁️',
-    '143': '🌫',
-    '176': '🌦',
-    '179': '🌧',
-    '182': '🌧',
-    '185': '🌧',
-    '200': '⛈',
-    '227': '🌨',
-    '230': '❄️',
-    '248': '🌫',
-    '260': '🌫',
-    '263': '🌦',
-    '266': '🌦',
-    '281': '🌧',
-    '284': '🌧',
-    '293': '🌦',
-    '296': '🌦',
-    '299': '🌧',
-    '302': '🌧',
-    '305': '🌧',
-    '308': '🌧',
-    '311': '🌧',
-    '314': '🌧',
-    '317': '🌧',
-    '320': '🌨',
-    '323': '🌨',
-    '326': '🌨',
-    '329': '❄️',
-    '332': '❄️',
-    '335': '❄️',
-    '338': '❄️',
-    '350': '🌧',
-    '353': '🌦',
-    '356': '🌧',
-    '359': '🌧',
-    '362': '🌧',
-    '365': '🌧',
-    '368': '🌨',
-    '371': '❄️',
-    '374': '🌧',
-    '377': '🌧',
-    '386': '⛈',
-    '389': '🌩',
-    '392': '⛈',
-    '395': '❄️'
+    '113': '',
+    '116': '',
+    '119': '',
+    '122': '',
+    '143': '',
+    '176': '󰼳',
+    '179': '󰼴',
+    '182': '󰼵',
+    '185': '󰖗',
+    '200': '',
+    '227': '',
+    '230': '',
+    '248': '',
+    '260': '',
+    '263': '',
+    '266': '',
+    '281': '',
+    '284': '',
+    '293': '󰖗',
+    '296': '󰖗',
+    '299': '',
+    '302': '',
+    '305': '',
+    '308': '',
+    '311': '',
+    '314': '',
+    '317': '',
+    '320': '',
+    '323': '',
+    '326': '',
+    '329': '',
+    '332': '',
+    '335': '',
+    '338': '',
+    '350': '󰼩',
+    '353': '',
+    '356': '',
+    '359': '',
+    '362': '',
+    '365': '',
+    '368': '󰖘',
+    '371': '',
+    '374': '',
+    '377': '',
+    '386': '',
+    '389': '',
+    '392': '',
+    '395': ''
 }
 
 data = {}
 
 
-weather = requests.get("https://wttr.in/Springfield, Manitoba?format=j1").json()
+weather = requests.get("https://wttr.in/Potenza, Italy?format=j1").json()
 
 
 def format_time(time):
@@ -87,12 +87,16 @@ def format_chances(hour):
             conditions.append(chances[event]+" "+hour[event]+"%")
     return ", ".join(conditions)
 
+tempint = int(weather['current_condition'][0]['FeelsLikeC'])
+extrachar = ''
+if tempint > 0 and tempint < 10:
+    extrachar = '+'
+
 
 data['text'] = WEATHER_CODES[weather['current_condition'][0]['weatherCode']] + \
-    " " + weather['current_condition'][0]['FeelsLikeC']+ "°"
-#data['text'] = weather['current_condition'][0]['FeelsLikeC']+"°"
+    " "+extrachar+weather['current_condition'][0]['FeelsLikeC']+"°"
 
-data['tooltip'] = f"<b>{weather['current_condition'][0]['weatherDesc'][0]['value']} {weather['current_condition'][0]['temp_C']}°</b>\n"
+data['tooltip'] = f"<b>{weather['current_condition'][0]['weatherDesc'][0]['value']} {weather['current_condition'][0]['temp_F']}°</b>\n"
 data['tooltip'] += f"Feels like: {weather['current_condition'][0]['FeelsLikeC']}°\n"
 data['tooltip'] += f"Wind: {weather['current_condition'][0]['windspeedKmph']}Km/h\n"
 data['tooltip'] += f"Humidity: {weather['current_condition'][0]['humidity']}%\n"
@@ -103,13 +107,13 @@ for i, day in enumerate(weather['weather']):
     if i == 1:
         data['tooltip'] += "Tomorrow, "
     data['tooltip'] += f"{day['date']}</b>\n"
-    data['tooltip'] += f"⬆️ {day['maxtempC']}° ⬇️ {day['mintempC']}° "
-    data['tooltip'] += f" {day['astronomy'][0]['sunrise']}  {day['astronomy'][0]['sunset']}\n"
+    data['tooltip'] += f" {day['maxtempC']}°  {day['mintempC']}° "
+    data['tooltip'] += f" {day['astronomy'][0]['sunrise']}  {day['astronomy'][0]['sunset']}\n"
     for hour in day['hourly']:
         if i == 0:
             if int(format_time(hour['time'])) < datetime.now().hour-2:
                 continue
-        data['tooltip'] += f"{format_time(hour['time'])} {WEATHER_CODES[hour['weatherCode']]} {format_temp(hour['FeelsLikeC'])} {hour['weatherDesc'][0]['value']}, {format_chances(hour)}\n"
+        data['tooltip'] += f"{format_time(hour['time'])} {WEATHER_CODES[hour['weatherCode']]} {format_temp(hour['FeelsLikeF'])} {hour['weatherDesc'][0]['value']}, {format_chances(hour)}\n"
 
 
 print(json.dumps(data))
