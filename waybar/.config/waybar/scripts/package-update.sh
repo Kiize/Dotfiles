e87ec5008e4bd69e05ed163125bad42391620587
Kiize <vitogallo99@gmail.com>
2025-12-20 10:12:29 +0100

    stow waybar

--- waybar/.config/waybar/scripts/package-update.sh
+++ waybar/.config/waybar/scripts/package-update.sh
@@ -12,15 +12,15 @@ updates_aur=$(yay -Qum 2>/dev/null | wc -l)
 updates=$((updates_arch + updates_aur))
 
 list_arch=$(checkupdates 2>/dev/null)
-list_yay=$(yay -Qum 2>/dev/null)
+list_aur=$(yay -Qum 2>/dev/null)
 
 if [ "$updates" -gt 0 ]; then
     num="($(format "$updates_arch")/$(format "$updates_aur"))"
     # lista unita con newline reali
-    tooltip=$(printf "%s\n%s" "$list_arch" "$list_yay")
+    tooltip=$(printf "%s\n%s" "$list_arch" "$list_aur")
 else
     num="Up to date."
-    tooltip="-"
+    tooltip="Up to date"
 fi
 
 echo -e "$num \n$tooltip"
